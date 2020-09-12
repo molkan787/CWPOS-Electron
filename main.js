70307c66f90e57bd3883bdaf8e2a99ffcd77d1c3
Dahmane Seghier <worw787@gmail.com>
2020-09-12 13:26:14 +0100

    Increased version number

--- main.js
+++ main.js
@@ -2,41 +2,47 @@ const { app, BrowserWindow } = require('electron')
 const DownloadManager = require("electron-download-manager")
 const server = require('./server');
 
-const port = 8083;
+const DEV = false;
+global.DEV = DEV;
+
+const port = DEV ? 8080 : 8083;
 
 let win
 
-function createWindow () {
-  win = new BrowserWindow({ width: 800, height: 600, 'min-width': 1000, 'min-height': 600, webPreferences: {
-    nodeIntegration: true
-  }, show: false});
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800, height: 600, 'min-width': 1000, 'min-height': 600, webPreferences: {
+      nodeIntegration: true
+    }, show: false
+  });
   win.setMenu(null);
 
   win.loadURL(`http://localhost:${port}/`)
 
-  win.webContents.on('did-finish-load', function() {
+  win.webContents.on('did-finish-load', function () {
     win.maximize();
     win.setMinimumSize(1000, 600);
     win.show();
   });
 
-  // win.webContents.openDevTools()
-  
+  if (DEV) win.webContents.openDevTools()
+
+
   win.on('closed', () => {
     win = null;
     app.quit();
   })
 }
 
-function init(){
-  DownloadManager.register({downloadFolder: app.getPath("desktop") + "/APOS-Reports"})
-  try {
-    server.listen(port, function() {
+function init() {
+  DownloadManager.register({ downloadFolder: app.getPath("desktop") + "/APOS-Reports" })
+  if (DEV) {
+    createWindow();
+  } else {
+    server.listen(port, function () {
       console.log('%s listening at %s', server.name, server.url);
       createWindow();
     });
-  } catch (error) {
-    alert('An error occured when starting the software. Error code: MP01');
   }
 }
 
